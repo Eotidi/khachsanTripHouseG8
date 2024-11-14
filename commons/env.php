da5188ce27ae34694265faddd5b8298a0b363cdb
Eotidi <l.t.dai710@gmail.com>
2024-11-14 15:52:29 +0700

    DaiCommit

--- commons/env.php
+++ commons/env.php
@@ -3,14 +3,15 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 // Duong dan den phan client 
-define('BASE_URL', 'http://localhost/khachsanTripHouse/');
+define('BASE_URL', 'http://localhost/khachsanTripHouseG8/');
 // Duong dan den phan admin
-define('BASE_URL_ADMIN', 'http://localhost/khachsanTripHouse/admin/');
+define('BASE_URL_ADMIN', 'http://localhost/khachsanTripHouseG8/admin/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , '');  // Tên database
+define('DB_NAME'    , 'triphousedb');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
+//TDcommit

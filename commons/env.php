2c7bc3e7862d1c4d0667c4e04da1455a8407518d
Minq05 <hoangquan22022005@gmail.com>
2024-11-14 15:51:49 +0700

    MQ

--- commons/env.php
+++ commons/env.php
@@ -3,9 +3,9 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 // Duong dan den phan client 
-define('BASE_URL', 'http://localhost/khachsanTripHouse/');
+define('BASE_URL', 'http://localhost/khachsanTripHouseG8/');
 // Duong dan den phan admin
-define('BASE_URL_ADMIN', 'http://localhost/khachsanTripHouse/admin/');
+define('BASE_URL_ADMIN', 'http://localhost/khachsanTripHouseG8/admin/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);

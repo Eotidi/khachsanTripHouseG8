efbd82c39eb421d16e7c02ef997b29dd78254b6c
Minq05 <hoangquan22022005@gmail.com>
2024-11-21 15:10:44 +0700

    Font Awesome - Quan

--- admin/views/layout/header.php
+++ admin/views/layout/header.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="https://png.pngtree.com/png-clipart/20190520/original/pngtree-hotel-icon-png-image_3570110.jpg">
   <title>GROUP 8 | TripHouse</title>
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="./assets/plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="./assets/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  
+  <div class="wrapper">
